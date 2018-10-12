cf099bb4a1a880d873415b0715857654a54403c6
went.out <went.out@gmail.com>
2018-10-13 02:55:46 +0300

    1.1.8 rid of postinstall hook

--- dist/rules/no-arrow-this.js
+++ dist/rules/no-arrow-this.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = context => {
+module.exports = (context) => {
 	// lowercased name of AST Arrow Function
 	const AST_AF = 'ArrowFunctionExpression'.toLowerCase();
 	// lowercased name of AST Program Start
@@ -9,29 +9,37 @@ module.exports = context => {
 	// Here are list of nodes for check breaking
 	// If one of them found, then everything is good
 	// And it is not necessary to check anymore else
-	const breakList = ['FunctionDeclaration', 'FunctionExpression', AST_PRG].map(name => name.toLowerCase());
-
+	const breakList = [
+		'FunctionDeclaration',
+		'FunctionExpression',
+		AST_PRG
+	].map(name => name.toLowerCase());
+	
 	// This options allows to warn only situations
 	// When context of arrow function 
 	// points Strict to Global or Window
-	const onlyGlobals = Array.isArray(context.options) && context.options[0] && context.options[0].onlyGlobals ? true : false;
-
+	const onlyGlobals = (
+		Array.isArray(context.options) &&
+		context.options[0] &&
+		context.options[0].onlyGlobals
+	) ? true : false;
+	
 	// Warn message depends of onlyGlobals option
 	const name = onlyGlobals ? 'global "this"' : '"this"';
-
+	
 	var type, parent;
-
+	
 	// checking function depends of onlyGlobals
-	const prepareCheckFn = node => {
+	const prepareCheckFn = (node) => {
 		parent = node;
-
+		
 		const switchParent = () => {
 			// switching AST node
 			// upper from current
 			parent = parent.parent;
 			type = parent.type.toLowerCase();
 		};
-
+		
 		if (!onlyGlobals) {
 			return () => {
 				switchParent();
@@ -44,20 +52,21 @@ module.exports = context => {
 				return false;
 			};
 		}
-
+		
 		let found = false;
 		return () => {
 			switchParent();
 			if (breakList.includes(type)) {
-				return found && type === AST_PRG ? true : null;
+				return (found && type === AST_PRG) ? true : null;
 			}
 			if (type === AST_AF) {
 				found = true;
 			}
 			return false;
 		};
+		
 	};
-
+	
 	return {
 		ThisExpression(node) {
 			const check = prepareCheckFn(node);
